b2b938e005bd51fed96927edf628f9bf929d9f5b
vargasova-oi <ulola@mail.ru>
2021-06-30 11:26:15 +0300

    Task 4

--- Task_2.py
+++ Task_2.py
@@ -1,1 +1,5 @@
-print("Hello!")
+sec = int(input("Укажите время в секундах: "))
+hour = sec // 3600
+mi = (sec % 3600) // 60
+sec = (sec % 3600) % 60
+print(f"Сейчас: {hour:2}:{mi:2}:{sec:2}")
---
+++ Task_3.py
@@ -0,0 +1,6 @@
+n = int(input("Укажите число n: "))
+nn = int(str(n) + str(n))
+nnn = int(str(n) + str(n) + str(n))
+summa = n + nn + nnn
+print(f"Сумма n + nn + nnn = {n} + {nn} + {nnn} = {summa}")
+# Выполнено
