fbbbd03ad16d9bf87e4c2113355b6c5e5d972478
vargasova-oi <ulola@mail.ru>
2021-06-30 13:41:31 +0300

    Task 4

--- Task_3.py
+++ Task_3.py
@@ -1,6 +1,6 @@
+# Выполнено
 n = int(input("Укажите число n: "))
 nn = int(str(n) + str(n))
 nnn = int(str(n) + str(n) + str(n))
 summa = n + nn + nnn
 print(f"Сумма n + nn + nnn = {n} + {nn} + {nnn} = {summa}")
-# Выполнено
---
+++ Task_4.py
@@ -0,0 +1,11 @@
+# Выполнено
+c = p
+Max = (p // 10) % 10
+while p > 10:
+    p1 = p % 10
+    if p1 >= Max:
+        Max = p1
+    p = p // 10
+    if Max == 9:
+        break
+print(f"Самая большая цифра в числе {c} это {Max}")
